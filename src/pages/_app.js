cb6de056894e004eaf70b826909440c2ed8b64e8
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2023-02-25 16:47:49 +0500

    upload files

---
+++ src/pages/_app.js
@@ -0,0 +1,6 @@
+import path from "path";
+import "../styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "@/components/Navbar";
+// import "@/app/global.css";
+
+const home = ({ children }) => {
+  return (
+    <div className="text-2xl font-bold text-red-700 test">
+      Test
+      <p className="test">Test</p>
+    </div>
+  );
+};
+
+export default home;
